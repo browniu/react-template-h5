917e0dd76d8b4c76875e955fc1d98168c7dd318f
browniu <niuniusoga@qq.com>
2019-10-13 00:55:22 +0800

    加入fullpage插件

--- src/pages/root/index.jsx
+++ src/pages/root/index.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import styles from './styles.module.scss'
-
+import fullpage from 'fullpage.js'
 export default class Components extends Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
+    componentDidMount() {
+        new fullpage('#fullpage', {
+            //options here
+            autoScrolling: true,
+            scrollHorizontally: true
+        });
+    }
     render() {
         return (
             <div className={styles.root}>
-                xixi
+                <div id="fullpage">
+                    <div class="section">Page1</div>
+                    <div class="section">Page2</div>
+                    <div class="section">Page3</div>
+                    <div class="section">Page4</div>
+                </div>
             </div>
         )
     }
