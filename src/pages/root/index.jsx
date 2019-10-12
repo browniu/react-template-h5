1ef6e3d80a17ba1dc9f824caebe44b9c5d023dff
browniu <niuniusoga@qq.com>
2019-10-13 00:27:00 +0800

    基本页面结构

---
+++ src/pages/root/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from './styles.module.scss'
+
+export default class Components extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className={styles.root}>
+                xixi
+            </div>
+        )
+    }
+}
